cd153aaa380008d70182063a91610d07b201ad24
Ali BOYALI <boyali@gmail.com>
2015-05-26 19:25:25 +0900

    Update test_matMyo.m

--- test_matMyo.m
+++ test_matMyo.m
@@ -1,9 +1,19 @@
 
-% Gestures
-% Ref_Fist(Fist Reference). fist (relax-fist), cc (come come). double (click)
-% Thumb, wavein, waveout
+ 
  clear
  clc
  f=myo_2c_sfunc ;
  
- save rest_2
+ 
+ 
+ %%
+[~, ind]=unique(f.eCounter);  
+f.emgData =f.emgData(ind, :); 
+
+[~, ind]=unique(f.aCounter);  
+f.accData =f.accData(ind, :); 
+
+[~, ind]=unique(f.aCounter);  
+f.gyroData =f.gyroData(ind, :); %  save fist_1
+ 
+% myo poses; 0-unknown, 1-rest, 2- fist, 3- win, 4-wout, 5=fspread, 6- db
